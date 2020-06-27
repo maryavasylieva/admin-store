31b84cfdeb4af5243f41704a99c1b43b3d164465
maryavasylieva <miheta.laurelin@gmail.com>
2020-06-28 00:34:08 +0300

    Updates

---
+++ precache-manifest.ac64b84553333f294f343b3881c77e3c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3621930cd4a57d2104c1e9d8779de56d",
+    "url": "/admin-art-store/index.html"
+  },
+  {
+    "revision": "2ec0284759ba23bd2a47",
+    "url": "/admin-art-store/static/css/3.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "ac17298a2d3e4b484b72",
+    "url": "/admin-art-store/static/css/4.fd33ede6.chunk.css"
+  },
+  {
+    "revision": "b22286b4b48cef032cc9",
+    "url": "/admin-art-store/static/js/2.17ee67ba.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/admin-art-store/static/js/2.17ee67ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec0284759ba23bd2a47",
+    "url": "/admin-art-store/static/js/3.8c572978.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/admin-art-store/static/js/3.8c572978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac17298a2d3e4b484b72",
+    "url": "/admin-art-store/static/js/4.7af294e0.chunk.js"
+  },
+  {
+    "revision": "0b25e6f61d60569dbebc",
+    "url": "/admin-art-store/static/js/5.6f9a86fa.chunk.js"
+  },
+  {
+    "revision": "bc98aaab9830247b6921",
+    "url": "/admin-art-store/static/js/main.797e3a16.chunk.js"
+  },
+  {
+    "revision": "da161e4f599b3f6515f8",
+    "url": "/admin-art-store/static/js/runtime-main.1153544a.js"
+  },
+  {
+    "revision": "60b386308fb9c6f3641a6c8acc9a5f3c",
+    "url": "/admin-art-store/static/media/ArtistShopLogo.60b38630.png"
+  },
+  {
+    "revision": "7895de41183df2a9c02cd003d7cb0ca5",
+    "url": "/admin-art-store/static/media/alois-arnegger-avstriiskii-zhivopisets-alois-arnegger-aus-12.7895de41.jpg"
+  }
+]);
