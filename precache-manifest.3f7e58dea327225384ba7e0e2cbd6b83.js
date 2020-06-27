019629b504a6fdbea6bd63e09960908c368bab2e
maryavasylieva <miheta.laurelin@gmail.com>
2020-06-28 00:43:39 +0300

    Updates

--- precache-manifest.3f7e58dea327225384ba7e0e2cbd6b83.js
+++ precache-manifest.3f7e58dea327225384ba7e0e2cbd6b83.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef720b6ab823b87a59799c38771add86",
+    "revision": "4d6c6bedb8533a479afaf9a44c1fc5ee",
     "url": "/admin-art-store/index.html"
   },
   {
-    "revision": "2ec0284759ba23bd2a47",
+    "revision": "6c3907b8c9f4ecd8537e",
     "url": "/admin-art-store/static/css/3.63b65d4e.chunk.css"
   },
   {
@@ -20,12 +20,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/admin-art-store/static/js/2.17ee67ba.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2ec0284759ba23bd2a47",
-    "url": "/admin-art-store/static/js/3.8c572978.chunk.js"
+    "revision": "6c3907b8c9f4ecd8537e",
+    "url": "/admin-art-store/static/js/3.f343437f.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/admin-art-store/static/js/3.8c572978.chunk.js.LICENSE.txt"
+    "url": "/admin-art-store/static/js/3.f343437f.chunk.js.LICENSE.txt"
   },
   {
     "revision": "ac17298a2d3e4b484b72",
@@ -36,8 +36,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/admin-art-store/static/js/5.6f9a86fa.chunk.js"
   },
   {
-    "revision": "06e6f40ffd92a208adf2",
-    "url": "/admin-art-store/static/js/main.0ed38bba.chunk.js"
+    "revision": "7d53f2e91e641cdfaa8c",
+    "url": "/admin-art-store/static/js/main.d9d1d144.chunk.js"
   },
   {
     "revision": "da161e4f599b3f6515f8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/admin-art-store/precache-manifest.d8ceee33df79813d35e7a7f310537e01.js"
+  "/admin-art-store/precache-manifest.3f7e58dea327225384ba7e0e2cbd6b83.js"
 );
 
 self.addEventListener('message', (event) => {
